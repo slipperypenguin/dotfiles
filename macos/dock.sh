938905ef27c765924d3f2297504d34698c64ccd1
Brady Paterson <brady.paterson@ibm.com>
2018-01-02 20:34:24 -0500

    configure default dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 #dockutil --no-restart --remove all
-#dockutil --no-restart --add "/Applications/Google Chrome.app"
+#dockutil --no-restart --add "/Applications/Launchpad.app"
+#dockutil --no-restart --add "/Applications/App Store.app"
 #dockutil --no-restart --add "/Applications/Mail.app"
+#dockutil --no-restart --add "/Applications/FirefoxDeveloperEdition.app"
+#dockutil --no-restart --add "/Applications/Safari.app"
 #dockutil --no-restart --add "/Applications/Calendar.app"
-#dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+#dockutil --no-restart --add "/Applications/Messages.app"
+#dockutil --no-restart --add "/Applications/Reminders.app"
+#dockutil --no-restart --add "/Applications/Notes.app"
+#dockutil --no-restart --add "/Applications/iTunes.app"
 #dockutil --no-restart --add "/Applications/System Preferences.app"
-#dockutil --no-restart --add "/Applications/Spotify.app"
+#dockutil --no-restart --add "/Applications/Evernote.app"
 
 #killall Dock
